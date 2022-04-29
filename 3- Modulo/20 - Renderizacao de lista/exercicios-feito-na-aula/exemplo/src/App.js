001195f99d0277c4fe6c27290dfe9f26866eac98
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-04-29 01:42:02 -0300

    entendendo a aula

---
+++ 3- Modulo/20 - Renderizacao de lista/exercicios-feito-na-aula/exemplo/src/App.js
@@ -0,0 +1,84 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import React from "react";
+import styled from "styled-components"
+
+const CardFamilia = styled.li`
+border:2px solid;
+border-image: linear-gradient( 50deg, red, blue) 2;
+width: 300px;
+display:flex ;
+justify-content:space-around;
+align-items: center ;
+height: 30px;
+list-style: none;
+font-size:20px;
+margin-bottom:10px;
+background-color: pink;
+`
+// const CardFamilia = styled.li:hover`
+// background-color: red;
+// `
+const CardIdade = styled.li `
+color: black;
+`
+
+class App extends React.Component {
+
+
+  state = {
+
+    meusIrmaos : [
+      {nome: 'Zeferina', idade:38},
+      {nome: 'Nico',     idade:36},
+      {nome: 'Seceu',    idade:34},
+      {nome: 'Mabele',   idade:32},  
+    ]
+  }
+
+  
+  minhaFamilia = this.state.meusIrmaos.map((familia) => {
+    return(
+      <CardFamilia>
+       <li key={familia}> {familia.nome} </li>
+       <CardIdade> {familia.idade} </CardIdade>
+     </CardFamilia>
+    ) 
+  })
+  
+  render(){
+  
+    return (
+  
+      <div>
+        <h1>Meus Famailiares</h1>
+        <p> {this.minhaFamilia} </p>
+      </div>
+    );
+  }
+
+}
+  
+  export default App
+  
+  
+
+
+//   listaComponentes = [
+//     "Item 1 ",
+//     "Item 2 ",
+//     "Item 3 "
+//   ]
+
+//   listaDados = this.listaComponentes.map(( dado ) => {
+//     return <li key={dado}> { dado } </li>
+//   })
+
+//   render() {
+
+//     return (
+//       <div> 
+//         <ul> { this.listaDados } </ul>
+//       </div>
+//     );
+//   }
+// };
---
+++ 3- Modulo/20 - Renderizacao de lista/exercicios-feito-na-aula/exemplo/src/segundo.js
@@ -0,0 +1,34 @@
+// import React from "react";
+
+// class App extends React.Component {
+
+//   state = {
+
+//     meusIrmaos : [
+//       {nome: 'Zeferina', idade:38},
+//       {nome: 'Nico',     idade:36},
+//       {nome: 'Seceu',    idade:34},
+//       {nome: 'Mabele',   idade:32},  
+//     ]
+//   }
+
+  
+//   minhaFamilia = this.state.meusIrmaos.map((familia) => {
+//     return <p key={irmaos}> {familia} </p>
+//   })
+  
+//   render(){
+  
+//     return (
+  
+//       <div>
+//         <h1>Meus Famailiares</h1>
+//         {/* <p> {this.minhaFamilia} </p> */}
+//       </div>
+//     )
+//   }
+
+// }
+
+// export default App
+
