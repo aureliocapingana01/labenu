d7630482779b56fa5b6a2a346d666a2e8b1993a8
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-05-22 21:28:14 -0300

    excelente aula

---
+++ 3- Modulo/27 - Integração básica de APIs/exercicio-em-aula/src/App.js
@@ -0,0 +1,15 @@
+import IntegracaoApi from '../src/Components/IntegracaoApi'
+
+function App() {
+  
+
+  return(
+    <>
+       <IntegracaoApi>
+         
+       </IntegracaoApi>
+    </>
+  )
+}
+
+export default App;
