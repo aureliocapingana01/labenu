ea816fb964c8c9897302dba968a72f9de2bdfb8b
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-08-06 18:42:57 -0300

    exercicio feito na primeira aula de back end

--- Back-End/1 - Aula/index.js
+++ Back-End/1 - Aula/index.js
@@ -1,4 +1,39 @@
 // console.log("Hello World")
 
-const nome = process.argv["3"]
-console.log(`para os programadores, ${nome}`)
+import { pessoas } from "./pessoas.js";
+
+const nomeBusca = process.argv[2]
+
+// const resultado = pessoas.find((nome) => nome === nomeBusca)
+
+const resultado = pessoas.filter((nome) => nome.toLowerCase().includes(nomeBusca.toLowerCase()))
+
+console.table(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const nome = process.argv["3"]
+// console.log(`para os programadores, ${nome}`)
---
+++ Back-End/1 - Aula/pessoas.js
@@ -0,0 +1,3 @@
+export const pessoas = [
+    'Aurelio', 'Nico', 'Zeferina', 'Seceu', 'Ana'
+]
