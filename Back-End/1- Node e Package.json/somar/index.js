00ecf15ff707fbb328655a3820f57ef9b17f08b5
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-09-07 16:58:16 -0300

    aula feita

--- Back-End/1- Node e Package.json/somar/index.js
+++ Back-End/1- Node e Package.json/somar/index.js
@@ -1,4 +1,16 @@
+import { pessoas } from "./pessoas.js";
+
+const nomeBusca = process.argv[2]
+// const resultado = pessoas.find((nome) => nome === nomeBusca)
+const resultado = pessoas.filter((nome) => nome.toLocaleUpperCase().includes(nomeBusca.toLocaleUpperCase()))
+console.table(resultado)
+
+
+
+
+
+
 // console.log("Hello World")
 
-const nome = process.argv["3"]
-console.log(`para os programadores, ${nome}`)
+// const nome = process.argv["2"]
+// console.log(`olá, ${nome}`)
---
+++ Back-End/1- Node e Package.json/somar/pessoas.js
@@ -0,0 +1,3 @@
+ 
+ 
+export const pessoas = ['Aurélio', 'Castro', 'Ana', 'Zeferina', 'Seceu']
