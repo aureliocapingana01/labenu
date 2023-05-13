629146b89dd8250bbd33c16f53bc88f8a33e8dd1
Aurélio Capingana <94324791+aureliocapingana01@users.noreply.github.com>
2023-05-13 00:22:25 -0300

    Delete Front-End directory

--- Front-End/1- Modulo/03 - aula-de-variavel/programa/programa.js
+++
@@ -1,9 +0,0 @@
-prompt("hoje é segunda-feira?")
-prompt("falo apartir de que Brasil?")
-prompt("A cor da minha roupa é azul?")
-
-let dia = "Sexta-Feira"
-let país = "Brasil"
-let cor =  "Verde"
-
-document.write('Hoje é ' + dia +',  falo apartir do , ' + país + ' a cor da minha roupa é ' + cor + '; obrigado!!!' )
--- Front-End/Projetos/cookenu/src/constants/colors.js
+++
@@ -1,2 +0,0 @@
-export const primaryColor = "#8C0E03"
-export const secundaryColor = "#D92B04"
