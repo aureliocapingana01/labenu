521567209c5dde432738155754ecc27c5d6eb6d1
Aurelio Capingana <aurelio.capingana01@gmail.com>
2023-08-01 23:34:26 -0300

    aprendendo array

--- 1-frontEnd/1_Modulo_Git_logica_javascrpt/bloco2/3-lacos/exer1/exer1.js
+++ 1-frontEnd/1_Modulo_Git_logica_javascrpt/bloco2/3-lacos/exer1/exer1.js
@@ -4,19 +4,29 @@
     b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array
     c) Por fim, imprima o array com os nomes dos bichinhos no console
  */
-
-let qtAniamais = Number(prompt('Quantos animais voce tem: '))
+let input = require("readline-sync");
+let qtAniamais = Number(input.question("Quantos animais voce tem: "));
 // let qtAniamais = 2
-let nomeDosAnimais = []
-let nome = [] 
+let nomeDosAnimais = [];
+let nome;
 
-for (i = 0; i < qtAniamais; i++) {
-    if(qtAniamais > 0) {
-        nome = prompt('Digite os nomes: ')
-        nomeDosAnimais = nome
-        console.log(nomeDosAnimais)
-    }
-    else {
-        console.log("Que pena! Você pode adotar um pet!")
-    }
+if (qtAniamais !== 0) {
+  for (animais = 0; animais < qtAniamais; animais++) {
+    nome = input.question("Digite os nomes: ");
+    nomeDosAnimais += nome + ",";
+  }
+  console.table(nomeDosAnimais);
+} else {
+  console.log("Que pena, adote um por favor");
 }
+
+// for (i = 0; i < qtAniamais; i++) {
+//     if(qtAniamais > 0) {
+//         nome = prompt('Digite os nomes: ')
+//         nomeDosAnimais = nome
+//         console.log(nomeDosAnimais)
+//     }
+//     else {
+//         console.log("Que pena! Você pode adotar um pet!")
+//     }
+// }
