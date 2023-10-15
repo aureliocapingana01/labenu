157093860a2d1af6b71345d1540bc697263be15a
Aurelio Capingana <aurelio.capingana01@gmail.com>
2023-10-15 19:39:21 -0300

    exercicios usando o userEffect

--- 1-frontEnd/4_Modulo_React_Avançado_e_Hooks/bloco_1/2_Hooks_e_useState/hook_useeffect/src/App.js
+++ 1-frontEnd/4_Modulo_React_Avançado_e_Hooks/bloco_1/2_Hooks_e_useState/hook_useeffect/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Exercicio1 from "./Pages/Exercicio_1/Exercicio1";
+// import Exercicio1 from "./Pages/Exercicio_1/Exercicio1";
 import Exercicio2 from "./Pages/Exercicio_2/Exercicio2";
 
 function App() {
